1a891319e65fe2a3d23b95c239c5d656d94bd613
Javeria <javeria.qureshi10@gmail.com>
2022-12-13 08:04:13 -0500

    initial commit

---
+++ JsonFormat/JsonFormat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ObjectToJSONConversion
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Category { get; set; }
+        public string ProductName { get; set; }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Product prod = new Product() { ProductId = 101, Category = "Mobile Phone", ProductName = "Samsung" };
+
+            var opt = new JsonSerializerOptions() { WriteIndented = true };
+            string strJson = JsonSerializer.Serialize<Product>(prod, opt);
+            Console.WriteLine(strJson);
+        }
+    }
+}
